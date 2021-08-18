14bcaabf7281309d5e5f7f48cac9bc4d857e444d
Fábio Ferreira <fabiorcferreira@gmail.com>
2021-08-18 12:43:44 +0100

    feat(name): Adds GenerateName function
    Generates a new Name from a name string

--- resource/name.go
+++ resource/name.go
@@ -2,6 +2,7 @@ package resource
 
 import (
 	"strconv"
+	"strings"
 )
 
 type Name struct {
@@ -9,6 +10,13 @@ type Name struct {
 
 	Collection string
 	Id         int64
+}
+
+func GenerateName(rawName string) *Name {
+	nameParts := strings.Split(rawName, "/")
+	nameCollection := nameParts[0]
+	nameID, _ := strconv.ParseInt(nameParts[1], 10, 64)
+	return NewName(nameCollection, nameID)
 }
 
 func NewName(collection string, id int64) *Name {
