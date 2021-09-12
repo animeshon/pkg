c79f7d5cf107d5e5a472ced482c2f37a25f79cee
Christian Roggia <christian.roggia@gmail.com>
2021-09-12 19:03:11 +0200

    fix(validation): fix function name typo

--- validation/validation.go
+++ validation/validation.go
@@ -2,31 +2,12 @@ package validation
 
 import (
 	"net/url"
-	"unicode"
 
 	"github.com/gobwas/glob"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
-const (
-	UsernameMinLength = 3
-	UsernameMaxLength = 32
-
-	GivenNameMaxLength  = 32
-	FamilyNameMaxLength = 32
-)
-
-func isValidName(name string) bool {
-	for _, r := range name {
-		if unicode.IsSymbol(r) || unicode.IsControl(r) || !unicode.IsPrint(r) {
-			return false
-		}
-	}
-
-	return true
-}
-
-func ValidateResoureName(name, pattern string, fldPath *field.Path) ErrorList {
+func ValidateResourceName(name, pattern string, fldPath *field.Path) ErrorList {
 	allErrs := ErrorList{}
 
 	if len(name) == 0 {
