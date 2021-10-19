7a4c70d67ba9a1beec5c52fc487a0c9fd3f7cd1b
Christian Roggia <christian.roggia@gmail.com>
2021-10-19 19:45:20 +0200

    feat(resource): add groups to Identity API

--- resource/identity.go
+++ resource/identity.go
@@ -38,3 +38,35 @@ func UserFullName(name string) (*Name, bool) {
 
 	return UserName(strings.TrimPrefix(name, IdentityAPI))
 }
+
+func GroupName(name string) (*Name, bool) {
+	tokens := strings.Split(name, "/")
+	if len(tokens) != 2 {
+		return nil, false
+	}
+
+	if tokens[0] != "groups" {
+		return nil, false
+	}
+
+	groupId, err := strconv.ParseInt(tokens[1], 10, 64)
+	if err != nil {
+		return &Name{
+			collection: tokens[0],
+			id:         tokens[1],
+		}, true
+	}
+
+	return &Name{
+		collection: tokens[0],
+		id:         groupId,
+	}, true
+}
+
+func GroupFullName(name string) (*Name, bool) {
+	if !strings.HasPrefix(name, IdentityAPI) {
+		return nil, false
+	}
+
+	return GroupName(strings.TrimPrefix(name, IdentityAPI))
+}
--- resource/identity_test.go
+++ resource/identity_test.go
@@ -16,4 +16,13 @@ func TestIdentityAPI(t *testing.T) {
 	userFull, ok := UserFullName("//identity.animeapis.com/users/3134441396375598")
 	require.True(t, ok)
 	assert.Equal(t, user.String(), userFull.String())
+
+	group, ok := GroupName("groups/3134441396375598")
+	require.True(t, ok)
+	assert.Equal(t, "groups", group.collection)
+	assert.Equal(t, int64(3134441396375598), group.id)
+
+	groupFull, ok := GroupFullName("//identity.animeapis.com/groups/3134441396375598")
+	require.True(t, ok)
+	assert.Equal(t, group.String(), groupFull.String())
 }
