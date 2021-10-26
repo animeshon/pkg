2e87a1a4a1496d65eb05a158762416fcda9af545
Christian Roggia <christian.roggia@gmail.com>
2021-10-26 16:18:30 +0200

    feat(randutils): automatically seed random package

--- randutils/rand_string.go
+++ randutils/rand_string.go
@@ -11,6 +11,10 @@ const PoolAlphaLowercase = "abcdefghijklmnopqrstuvwxyz"
 const PoolAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const PoolAlphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 const (
 	letterIdxBits = 6                    // 6 bits to represent a letter index
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
